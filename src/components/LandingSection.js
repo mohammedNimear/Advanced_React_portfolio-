cc75ed3d95d8bce10f2a0c9dab3c9834afa22404
mohammedNimear <mohammedalhassannumair@gmail.com>
2023-02-26 00:04:23 +0200

    first commit

---
+++ src/components/LandingSection.js
@@ -0,0 +1,44 @@
+import React from "react"; 
+import { Avatar, Heading, VStack } from "@chakra-ui/react"; 
+import FullScreenSection from "./FullScreenSection"; 
+ 
+const greeting = "Hello, I am Mohammedal Hassan !";
+const bio1 = "A Fullstack Developer";
+const bio2 = "Specialized in React & NodeJS";
+
+const LandingSection = () => ( 
+ <FullScreenSection 
+   justifyContent="center" 
+   alignItems="center" 
+   isDarkBackground 
+   backgroundColor="#2A4365" 
+ > 
+   <VStack spacing={16}> 
+     <VStack spacing={4} alignItems="center"> 
+       <Avatar 
+         src="https://i.pravatar.cc/150?img=7" 
+         size="2xl" 
+         name="Your Name" 
+       /> 
+       <Heading as="h4" size="md" noOfLines={1}> 
+         {greeting} 
+       </Heading> 
+     </VStack> 
+     <VStack spacing={6}> 
+       <Heading as="h1" size="3xl" noOfLines={1}> 
+         {bio1} 
+       </Heading> 
+       <Heading as="h1" size="3xl" noOfLines={1}> 
+         {bio2} 
+       </Heading> 
+     </VStack> 
+   </VStack> 
+ </FullScreenSection> 
+); 
+ 
+export default LandingSection;
+
+
+
+
+
